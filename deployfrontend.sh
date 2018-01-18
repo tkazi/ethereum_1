a879178264c8fdf9d6b98c03db4d006235ab78ce
Taha <taha698@gmail.com>
2018-01-18 09:05:21 -0500

    few index.html modifications

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -1,5 +1,15 @@
+# rsync -r src/ docs/
+# rsync build/contracts/ChainList.json docs/
+# #echo "Ethereuem Application (solidity and truffle). Basic app to sell and buy items on the blockchain" >> README.md
+# git add README.md
+# git remote add origin https://github.com/tkazi/ethereum_1.git
+# git commit -m "Adding frontend files to Github pages"
+# git push -u origin master
+
+
 rsync -r src/ docs/
-rsync build/contracts/Chainlist.json docs/
+rsync build/contracts/ChainList.json docs/
+#echo "Ethereuem Application (solidity and truffle). Basic app to sell and buy items on the blockchain" >> README.md
 git add .
-git commit -m "Adding frontend files to Github pages"
+git commit -m "few index.html modifications"
 git push
